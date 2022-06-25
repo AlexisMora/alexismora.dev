169ef44ead537911ac54d4fcf51560c0d1b960c7
Alexis Mora <alexis.mora.sanchez@gmail.com>
2022-06-25 21:07:14 +0200

    feat: 🚀 liftoff 🚀

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex flex-col justify-center w-full h-screen max-w-3xl mx-auto space-y-2">
+    <div>
+      <h1 class="text-2xl">Index</h1>
+      <p class="text-lg">Here's what I've done so far:</p>
+    </div>
+    <ul class="mx-8 list-disc">
+      <li>
+        Created a new Nuxt 3 project
+      </li>
+      <li>
+        Added Tailwind
+      </li>
+      <li>
+        Created a new page
+      </li>
+    </ul>
+  </div>  
+</template>
