0bad0a9ca6f97d424edabf891636fd7e45f985ee
Alexis Mora <alexis.mora.sanchez@gmail.com>
2022-06-28 18:56:34 +0200

    feat: links

--- components/BaseLink.vue
+++ components/BaseLink.vue
@@ -3,7 +3,8 @@
     <a
       :href="props.url"
       type="button"
-      class="cursor-pointer	py-2 px-4 bg-teal-400 hover:bg-teal-500 focus:ring-teal-300 focus:ring-offset-teal-100 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
+      target="_blank"
+      class="cursor-pointer	py-2 px-4 bg-teal-400 hover:bg-teal-600 focus:ring-teal-300 focus:ring-offset-teal-100 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
     >
       <slot />
     </a>
