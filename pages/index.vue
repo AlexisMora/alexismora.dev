83e84a81f0103d9d9a0fab3f7f8f3b22e7e80a08
Alexis Mora <alexis.mora.sanchez@gmail.com>
2022-06-30 21:47:20 +0200

    feat: wip main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,39 @@
 <template>
-  <div
-    class="flex flex-col justify-center w-full h-screen max-w-3xl mx-auto font-sans"
-  >
-    <div class="flex flex-col justify-center text-center">
-      <div class="mx-auto">
-        <div id="title">
-          <h1 class="text-2xl font-bold">Alexis Mora</h1>
-          <p class="text-lg">Frontend Engineer</p>
-        </div>
-        <div class="mt-12 text-left">
-          <h3 class="font-light text-left">About</h3>
-          <p class="mt-6">
-            I'm a software developer focused on frontend & design.
-          </p>
-          <p class="mt-2">
-            most of the projects I do are with Vue with Typescript and
-            Django-restframework
-          </p>
-          <p class="mt-2">
-            Currently i'm building applications as a Frontend Engineer at
-            <a class="text-teal-400" href="https://www.apsl.net/en/">Apsl</a>
-          </p>
-        </div>
-        <div class="mt-12 text-left">
-          <h3 class="font-light text-left">Links</h3>
-          <div>
-            <LinksContainer />
+  <div>
+    <div
+      class="flex flex-col justify-center w-full h-screen max-w-3xl mx-auto font-sans"
+    >
+      <div class="flex flex-col justify-center text-center">
+        <div class="mx-auto">
+          <div id="title">
+            <h1 class="text-2xl font-bold">Alexis Mora</h1>
+            <p class="text-lg">Frontend Engineer</p>
+          </div>
+          <div class="mt-12 text-left">
+            <h3 class="font-light text-left">About</h3>
+            <p class="mt-6">
+              I'm a software developer focused on frontend & design.
+            </p>
+            <p class="mt-2">
+              most of the projects I do are with Vue with Typescript and
+              Django-restframework
+            </p>
+            <p class="mt-2">
+              Currently i'm building applications as a Frontend Engineer at
+              <a class="text-teal-400" href="https://www.apsl.net/en/">Apsl</a>
+            </p>
+          </div>
+          <div class="mt-12 text-left">
+            <h3 class="font-light text-left">Links</h3>
+            <div>
+              <LinksContainer />
+            </div>
+          </div>
+          <div class="mt-12 text-left">
+            <h3 class="font-light text-left">Some of my stuff</h3>
+            <div>
+              <LinksContainer />
+            </div>
           </div>
         </div>
       </div>
@@ -33,5 +41,4 @@
   </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
