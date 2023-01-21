f2e4753d764c00ecfa99102ac77f9c649da09442
Alexis Mora <alexis.mora.sanchez@gmail.com>
2023-01-21 19:45:55 +0100

    feat: nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'GT-America-CT': ['GT-America-CT'],
+
+      }
+    },
   },
   plugins: [],
 }
