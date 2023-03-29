03e1931b03178253bd8b6eb5ebde83f9d669ba9d
Alexis Mora <alexis.mora.sanchez@gmail.com>
2023-03-29 23:46:39 +0200

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,11 @@ module.exports = {
         primary: '#4A89F8',
         darkerPrimary: '#041F4E',
         dark: '#010814'
+      },
+      fontFamily: {
+        'GT-America-CT': ['GT-America-CT'],
+        'GT-America-Expanded': ['GT-America-Expanded'],
+        'GT-America-Standard': ['GT-America-Standard']
       }
     }
   },
